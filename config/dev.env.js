b55afbf282334b7d8ff8b2cfe92dcb153769899c
xuliang <510013803@qq.com>
2018-12-09 23:35:46 +0800

    modify

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,5 +1,5 @@
 module.exports = {
     NODE_ENV: '"development"',
-    BASE_API: '"http://127.0.0.1:8081"',
+    BASE_API: '"http://127.0.0.1:8769/beta-auth"',
     APP_ORIGIN: '"https://shizijie.com"'
 }
